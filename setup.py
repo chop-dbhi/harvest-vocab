3e5b00ab6ef5a2a36630700b58d524fbb16e2a1f
millerjm1@email.chop.edu <millerjm1@email.chop.edu>
2010-07-23 15:07:53 -0400

    Moving files into proper structure.

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+kwargs = {
+    'name': 'django-vocabulary',
+    'version': '.4',
+    'author': 'Jeff Miller',
+    'author_email': 'millerjm1@email.chop.edu',
+    'description': 'A vocabulary browser for django for tree structured data. Includes database back-end and front-end for AudGenDB framework.',
+    'license': 'BSD',
+    'keywords': 'AudGenDB avocado django vocabulary browser',
+    'install_requires': ['django>=1.2','django-avocado>=1.0'],
+    'package_data': {
+    
+    },
+    'packages': find_packages(exclude=('*.tests', '*.tests.*')),
+    'package_data': {
+        'avocado': ['sql/*.sql']
+    },
+    'exclude_package_data': {
+        '': ['fixtures/*']
+    },
+    'classifiers': [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+}
+
+setup(**kwargs)
+
+instructions = """This is a django application, and needs to be placed in the INSTALLED_APPS array in your settings.py file. It also has additional requirements:
+1. It has its own static files that must be made available. It looks in the static directory of your django project for plugins/vb/.
+2. This application has its own models and expects find those models in a database called "vocabulary" in the database dictionary of your settings file.
+"""
+print instructions
