763142880e83912d334d366013f12fe45e284496
millerjm1@email.chop.edu <millerjm1@email.chop.edu>
2010-07-23 16:21:11 -0400

    Update to instructions in setup.py

--- setup.py
+++ setup.py
@@ -29,5 +29,6 @@ setup(**kwargs)
 instructions = """This is a django application, and needs to be placed in the INSTALLED_APPS array in your settings.py file. It also has additional requirements:
 1. It has its own static files that must be made available. It looks in the static directory of your django project for plugins/vb/.
 2. This application has its own models and expects find those models in a database called "vocabulary" in the database dictionary of your settings file.
+3. The urls.py in this application needs to be included in the main urls.py for the project so that the urls.py is found at /plugins/vb/
 """
 print instructions
