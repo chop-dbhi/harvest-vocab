0c334fe0dbf8707e967418db47041f5dc0d7c7be
millerjm1@email.chop.edu <millerjm1@email.chop.edu>
2010-07-24 22:12:24 -0400

    Fixed setup.py to include static files.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ kwargs = {
     'keywords': 'AudGenDB avocado django vocabulary browser',
     'install_requires': ['django>=1.2','django-avocado>=1.0'],
     'packages': find_packages(exclude=('*.tests', '*.tests.*')),
-    'package_data':{'static':['*.js','*.css']},
+    'package_data':{'vocabulary':['static/js/*.js','static/css/*.css']},
     'exclude_package_data': {
         '': ['fixtures/*']
     },
