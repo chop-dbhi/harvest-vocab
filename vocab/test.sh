ff35b1c55b66b8feff40087b70366ce9c5843596
Byron Ruth <ruthb@email.chop.edu>
2011-06-29 09:55:20 -0400

    removing old test cases

--- vocab/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# ignore if this in the path, this is just a hack to make this play nicely with the different static paths
-python test/httpserver.py -d "/plugins/vb"&
-python -m webbrowser -t "http://localhost:8000/test/testdrive.html"
-
-
