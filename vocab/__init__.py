287f985d0c7e33199534cf44f1b06412efec8638
Byron Ruth <ruthb@email.chop.edu>
2011-06-29 10:00:35 -0400

    updated setup script

---
+++ vocab/__init__.py
@@ -0,0 +1,15 @@
+VERSION = (0, 9, 0, 'beta', 1)
+
+def get_version():
+    version = '%s.%s' % VERSION[:2]
+    if version[2]:
+        version = '%s.%s' % (version, VERSION[2])
+
+    if len(VERSION) > 3:
+        if VERSION[3:] == ('alpha', 0):
+            version = '%spre-alpha' % version
+        else:
+            if VERSION[3] != 'final':
+                version = '%s%s%s' % (version, VERSION[3], VERSION[4])
+    return version
+
