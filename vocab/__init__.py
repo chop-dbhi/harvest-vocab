de7b76a4744ecb50b68ae96ca1731dddc1fbb678
Byron Ruth <b@devel.io>
2012-10-08 15:52:52 -0400

    Bump version for the 2.x series of Harvest

--- vocab/__init__.py
+++ vocab/__init__.py
@@ -1,9 +1,9 @@
 __version_info__ = {
-    'major': 0,
-    'minor': 9,
-    'micro': 1,
+    'major': 2,
+    'minor': 0,
+    'micro': 0,
     'releaselevel': 'beta',
-    'serial': 9
+    'serial': 1
 }
 
 def get_version(short=False):
