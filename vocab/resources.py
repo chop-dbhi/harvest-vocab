97566d8d61647d296747c905c4a4319f8468d748
Jeffrey Miller <jeffmax@gmail.com>
2013-05-02 15:32:30 -0400

    Remove serrano from views.

--- vocab/resources.py
+++ vocab/resources.py
@@ -1,40 +1,57 @@
 from django.db.models import Q
-from serrano.resources.datafield.values import DataFieldValues
+from avocado.models import DataField
+from django.conf import settings
 from restlib2.http import codes
+from restlib2 import resource
 
-class ItemResource(DataFieldValues):
-    def get(self, request, pk):
-        item = self.get(request, pk=pk)
+class ItemResource(resource):
+    fields = (':pk', 'description->name', 'abbreviation', 'code', 'terminal',
+        'get_absolute_url->uri', 'children', 'ancestors')
+
+    def get(self, request, field_pk, pk):
+        if not field_pk in [str(x) for x in settings.VOCAB_FIELDS]:
+            return codes.NOT_FOUND
+
+        field = DataField.objects.get(pk=field_pk)
+        model = field.model
+
+        item = model.objects.get(request, pk=pk)
 
         if item is None:
             return codes.NOT_FOUND
         return item
 
-class ItemResourceCollection(DataFieldValues):
+class ItemResourceCollection(resource):
     search_enabled = True
     max_results = 100
     order_by = None
 
-    def get(self, request):
-        queryset = self.queryset(request)
+    def get(self, request, field_pk):
+        if not field_pk in [str(x) for x in settings.VOCAB_FIELDS]:
+            return codes.NOT_FOUND
 
-        order_by = self.order_by
-        if not order_by:
-            order_by = (queryset.model.description_field,)
+        field = DataField.objects.get(pk=field_pk)
+        model = field.model
+
+        order_by = (model.description_field,)
 
         # search if enabled
-        if self.search_enabled and request.GET.has_key('q'):
+        if field.search_enabled and request.GET.has_key('q'):
             kwargs = {}
             q = request.GET['q']
             query = Q()
 
-            for field in getattr(self.model, 'search_fields', ()):
+            for field in getattr(model, 'search_fields', ()):
                 query = query | Q(**{'%s__icontains' % field: q})
 
             queryset = queryset.order_by('-parent', *order_by)
 
-            return queryset.filter(query)[:self.max_results]
+            return model.objects.filter(query)[:self.max_results] \
+                .order_by('-parent', *order_by)
 
         # get all root items by default
-        return queryset.filter(parent=None).order_by(*order_by)
+        return model.objects.filter(parent=None).order_by(*order_by)
+
+class Resources(resource):
+    pass
 
