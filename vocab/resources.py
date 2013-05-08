97fd3ed5b03d671ecf7d055db96f6bbbb71b5106
Jeffrey Miller <jeffmax@gmail.com>
2013-05-08 14:55:26 -0400

    Explicitly return HttpResponse.
    Properly catch ObjectDoesNotExist errors.

--- vocab/resources.py
+++ vocab/resources.py
@@ -1,26 +1,27 @@
 from django.db.models import Q
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
 from avocado.models import DataField
 from restlib2.http import codes
 from restlib2.resources import Resource
 from preserialize.serialize import serialize
 from .settings import VOCAB_FIELDS
-
+from django.http import HttpResponse
 class ItemResource(Resource):
     fields = (':pk', 'description->name', 'abbreviation', 'code', 'terminal',
         'get_absolute_url->uri', 'children', 'ancestors')
 
     def get(self, request, field_pk, pk):
         if not field_pk in [str(x) for x in VOCAB_FIELDS]:
-            return codes.NOT_FOUND
+            return HttpResponse(status=codes.not_found)
 
-        field = DataField.objects.get(pk=field_pk)
-        model = field.model
+        try:
+           field = DataField.objects.get(pk=field_pk)
+           model = field.model
+           item = model.objects.get(pk=pk)
+        except ObjectDoesNotExist:
+            return HttpResponse(status=codes.not_found)
 
-        item = model.objects.get(pk=pk)
-
-        if item is None:
-            return codes.NOT_FOUND
         return serialize(item)
 
 class ItemResourceCollection(Resource):
@@ -31,7 +32,7 @@ class ItemResourceCollection(Resource):
     def get(self, request, field_pk):
         uri = request.build_absolute_uri
         if not field_pk in [str(x) for x in VOCAB_FIELDS]:
-            return codes.NOT_FOUND
+            return HttpResponse(status=codes.not_found)
 
         field = DataField.objects.get(pk=field_pk)
         model = field.model
@@ -57,7 +58,7 @@ class ItemResourceCollection(Resource):
         # if no nodes have children, we return not found so the client
         # can react (go into search only mode)
         if model.objects.filter(children__isnull = False).count() == 0:
-            return codes.NOT_FOUND
+            return HttpResponse(status=codes.not_found)
         # get all root items by default
         return serialize(model.objects.filter(parent=None).order_by(*order_by))
 
