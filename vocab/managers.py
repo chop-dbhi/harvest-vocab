a2c45e37e301882db8753ccc8507ca52a5153861
Byron Ruth <b@devel.io>
2013-08-25 15:30:53 -0400

    Abstract out SQL templates
    All string formatting has been converted to the built-in 'advanced'
    formatting present in 2.6+.
    
    The true/false values have been removed from the case statement
    construction since the values were constant across operators.

--- vocab/managers.py
+++ vocab/managers.py
@@ -4,7 +4,27 @@ from django.db.models.sql import RawQuery
 
 qn = connection.ops.quote_name
 
-COLUMN_PREFIX = 'ct_'
+COLUMN_PREFIX = 'sc' # summed case
+
+CASE_STATEMENT_TEMPLATE = '''\
+SUM(
+    CASE
+        WHEN {index_table}.{item_column} = {item_pk} THEN 1
+        WHEN {index_table}.{parent_column} = {item_pk} THEN 1
+        ELSE 0
+    END
+) AS {column_alias}
+'''
+
+SELECT_STATEMENT_TEMPLATE = '''\
+SELECT {object_column} FROM (
+    SELECT {object_column}, {case_statements}
+    FROM {through_table}
+        INNER JOIN {index_table} ON ({through_table}.{item_column} = {index_table}.{item_id})
+    GROUP BY {object_column}
+) AS T WHERE {where_condition}
+'''
+
 
 class ItemManager(models.Manager):
     def ancestors(self, pk, *args, **kwargs):
@@ -37,42 +57,30 @@ class ItemThroughManager(models.Manager):
         "Reference to the item's `AbstractItemIndex` subclass."
         return self.item_field.rel.to.item_indexes.related.model
 
-    def _construct_case_statements(self, values, true_value, false_value):
-        "Constructs the case statements for the SQL string"
-        statement = '''
-            SUM(
-                CASE
-                    WHEN %(index_table)s.%(item_column)s = %(item_pk)s THEN %(true_value)s
-                    WHEN %(index_table)s.%(parent_column)s = %(item_pk)s THEN %(true_value)s
-                    ELSE %(false_value)s
-                END
-            ) AS %(column_alias)s
-        '''
-
-        query = [statement % {
+    def _construct_case_statements(self, values):
+        "Constructs a summed case statement SQL string for each value."
+        query = [CASE_STATEMENT_TEMPLATE.format(**{
             'item_pk': pk,
             'index_table': qn(self.index_model._meta.db_table),
             'item_column': qn('item_id'),
             'parent_column': qn('parent_id'),
-            'true_value': true_value,
-            'false_value': false_value,
             'column_alias': qn(COLUMN_PREFIX + str(pk)),
-        } for pk in values]
+        }) for pk in values]
         return ', '.join(query)
 
     def _construct_where_condition(self, values, where_equals, node_join):
         """Constructs the conditions portion of the SQL string by setting each
-        values row equal to `where_equals`.
+        values row equal to `where_equals` and join the conditions by
+        `node_join` either AND or OR.
         """
         if where_equals is True:
-            statement = ' %(column_alias)s > 0 '
+            statement = ' {column_alias} > 0 '
         else:
-            statement = ' %(column_alias)s = %(where_equals)s '
+            statement = ' {column_alias} = {where_equals} '
 
-        query = [statement % {
-            'column_alias': qn(COLUMN_PREFIX + str(pk)),
-            'where_equals': where_equals,
-        } for pk in values]
+        query = [statement.format(column_alias=qn(COLUMN_PREFIX + str(pk)),
+            where_equals=where_equals) for pk in values]
+
         return node_join.join(query)
 
     def _get_query(self, case_statements, where_conditions, evaluate):
@@ -80,14 +88,7 @@ class ItemThroughManager(models.Manager):
         and where conditions provided and runs the query on the associated
         model.
         """
-        query = '''
-            SELECT %(object_column)s FROM (
-                SELECT %(object_column)s, %(case_statements)s
-                FROM %(through_table)s
-                    INNER JOIN %(index_table)s ON (%(through_table)s.%(item_column)s = %(index_table)s.%(item_id)s)
-                GROUP BY %(object_column)s
-            ) AS T WHERE %(where_condition)s
-        ''' % {
+        query = SELECT_STATEMENT_TEMPLATE.format(**{
             'item_column': qn(self.item_field.column),
             'object_column': qn(self.object_field.column),
             'case_statements': case_statements,
@@ -95,18 +96,18 @@ class ItemThroughManager(models.Manager):
             'index_table': qn(self.index_model._meta.db_table),
             'where_condition': where_conditions,
             'item_id': qn('item_id'),
-        }
+        })
         # Clean up the whitespace
         query = re.sub('\s+', ' ', query)
         if evaluate:
             return [x[0] for x in RawQuery(query, using=self.db)]
-        return '(%s)' % query
+        return '({0})'.format(query)
 
     def requires_any(self, items, evaluate=False):
         "Returns through objects that are associated with 'any' of the given items."
         if type(items[0]) is not int:
             items = [term.pk for term in items]
-        case_statements = self._construct_case_statements(items, 1, 0)
+        case_statements = self._construct_case_statements(items)
         where_condition = self._construct_where_condition(items, True, 'OR')
         return self._get_query(case_statements, where_condition, evaluate)
 
@@ -114,7 +115,7 @@ class ItemThroughManager(models.Manager):
         "Returns through objects that are associated with 'all' of the given items."
         if type(items[0]) is not int:
             items = [term.pk for term in items]
-        case_statements = self._construct_case_statements(items, 1, 0)
+        case_statements = self._construct_case_statements(items)
         where_condition = self._construct_where_condition(items, True, 'AND')
         return self._get_query(case_statements, where_condition, evaluate)
 
@@ -122,14 +123,14 @@ class ItemThroughManager(models.Manager):
         "Returns through objects that are _not_ associated with all of the given items."
         if type(items[0]) is not int:
             items = [term.pk for term in items]
-        case_statements = self._construct_case_statements(items, 1, 0)
+        case_statements = self._construct_case_statements(items)
         where_condition = self._construct_where_condition(items, True, 'AND')
         return self._get_query(case_statements, where_condition, evaluate)
 
     def excludes_any(self, items, evaluate=False):
         if type(items[0]) is not int:
             items = [term.pk for term in items]
-        case_statements = self._construct_case_statements(items, 1, 0)
+        case_statements = self._construct_case_statements(items)
         where_condition = self._construct_where_condition(items, True, 'OR')
         return self._get_query(case_statements, where_condition, evaluate)
 
@@ -137,7 +138,7 @@ class ItemThroughManager(models.Manager):
         "Returns through objects that 'only' match the given items."
         if type(items[0]) is not int:
             items = [term.pk for term in items]
-        case_statements = self._construct_case_statements(items, 1, 0)
+        case_statements = self._construct_case_statements(items)
         where_condition = self._construct_where_condition(items, 1, 'AND')
         return self._get_query(case_statements, where_condition, evaluate)
 
@@ -153,7 +154,6 @@ class ItemIndexManager(models.Manager):
     @transaction.commit_on_success
     def index(self):
         indexed_model = self.model._meta.get_field_by_name('item')[0].rel.to
-
         items = indexed_model._default_manager.db_manager(self.db).all()
 
         for item in iter(items):
@@ -161,7 +161,6 @@ class ItemIndexManager(models.Manager):
 
         # reset terminal flag
         items.update(terminal=False)
-
         indexes = self.get_query_set()
 
         # terminals are defined as having no children, thus the index will not
